a0b623cdbcee8e29a8d0921ea7a553aa5095bf59
Kaiz_JP <kaiz@kaiz.jp>
2020-04-19 13:40:07 +0900

    TCCを乗車列車以外にも対応

--- src/main/java/jp/kaiz/atsassistmod/render/TrainGuiRender.java
+++ src/main/java/jp/kaiz/atsassistmod/render/TrainGuiRender.java
@@ -3,6 +3,7 @@ package jp.kaiz.atsassistmod.render;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import jp.kaiz.atsassistmod.api.TrainControllerClient;
+import jp.kaiz.atsassistmod.api.TrainControllerClientManager;
 import jp.ngt.rtm.entity.train.EntityTrainBase;
 import jp.ngt.rtm.modelpack.cfg.TrainConfig;
 import jp.ngt.rtm.modelpack.modelset.ModelSetVehicleBase;
@@ -15,125 +16,123 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 @SideOnly(Side.CLIENT)
 public class TrainGuiRender extends GuiScreen {
 
-	public TrainGuiRender(Minecraft mc) {
-		super();
-		this.mc = mc;
-	}
+    public TrainGuiRender(Minecraft mc) {
+        super();
+        this.mc = mc;
+    }
 
-	public void onRenderGui(RenderGameOverlayEvent event) {
-		if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
-			Minecraft mc = this.mc;
-			EntityPlayer player = mc.thePlayer;
-			if (!player.isRiding() || mc.gameSettings.thirdPersonView != 0) {
-				return;
-			}
+    public void onRenderGui(RenderGameOverlayEvent event) {
+        if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
+            Minecraft mc = this.mc;
+            EntityPlayer player = mc.thePlayer;
+            if (!player.isRiding() || mc.gameSettings.thirdPersonView != 0) {
+                return;
+            }
 
-			this.width = event.resolution.getScaledWidth();
-			this.height = event.resolution.getScaledHeight();
+            this.width = event.resolution.getScaledWidth();
+            this.height = event.resolution.getScaledHeight();
 
-			if (player.ridingEntity instanceof EntityTrainBase) {
-				if (((EntityTrainBase) player.ridingEntity).isControlCar()) {
+            if (player.ridingEntity instanceof EntityTrainBase) {
+                if (((EntityTrainBase) player.ridingEntity).isControlCar()) {
+                    this.renderTrainGui((EntityTrainBase) player.ridingEntity);
+                }
+            }
+        }
+    }
 
-					this.renderTrainGui((EntityTrainBase) player.ridingEntity);
-					return;
-				}
-			}
-			TrainControllerClient.setEntityID(0);
-		}
-	}
+    private void renderTrainGui(EntityTrainBase train) {
+        TrainControllerClient tcc;
+        if ((tcc = TrainControllerClientManager.getTCC(train)) != null) {
+            FontRenderer fontrenderer = this.mc.fontRenderer;
+            ModelSetVehicleBase<TrainConfig> model = train.getModelSet();
 
-	private void renderTrainGui(EntityTrainBase train) {
-		if (TrainControllerClient.isEnable(train.getEntityId())) {
-			FontRenderer fontrenderer = this.mc.fontRenderer;
-			ModelSetVehicleBase<TrainConfig> model = train.getModelSet();
-
-			int ato = TrainControllerClient.getATOSpeed();
-			int tasc = TrainControllerClient.getTASCDistance();
-			int atc = TrainControllerClient.getATCSpeed();
-			int atacs = TrainControllerClient.getATACSSpeed();
-			boolean atoStatus = TrainControllerClient.isATO();
-			boolean tascStatus = TrainControllerClient.isTASC();
-			boolean atacsStatus = TrainControllerClient.isATACS();
+            int ato = tcc.getATOSpeed();
+            int tasc = tcc.getTASCDistance();
+            int atc = tcc.getATCSpeed();
+            int atacs = tcc.getATACSSpeed();
+            boolean atoStatus = tcc.isATO();
+            boolean tascStatus = tcc.isTASC();
+            boolean atacsStatus = tcc.isATACS();
 
 
-			if (model != null && !model.getConfig().notDisplayCab) {
-				//cab表示あり
-				int k = this.width / 2;
+            if (model != null && !model.getConfig().notDisplayCab) {
+                //cab表示あり
+                int k = this.width / 2;
 
-				//ATO
-				if (atoStatus) {
-					fontrenderer.drawStringWithShadow("ATO : " + ato, k + 160, this.height - 40, 0x00FF00);
-				} else {
-					fontrenderer.drawStringWithShadow("ATO : off", k + 160, this.height - 40, 0x00FF00);
-				}
+                //ATO
+                if (atoStatus) {
+                    fontrenderer.drawStringWithShadow("ATO : " + ato, k + 160, this.height - 40, 0x00FF00);
+                } else {
+                    fontrenderer.drawStringWithShadow("ATO : off", k + 160, this.height - 40, 0x00FF00);
+                }
 
-				//TASC
+                //TASC
 //				fontrenderer.drawStringWithShadow("TASC : " + tasc, k + 160, this.height - 30, 0x00FF00);
-				if (tascStatus) {
-					fontrenderer.drawStringWithShadow("TASC : " + tasc, k + 160, this.height - 30, 0x00FF00);
-				} else {
-					fontrenderer.drawStringWithShadow("TASC : off", k + 160, this.height - 30, 0x00FF00);
-				}
+                if (tascStatus) {
+                    fontrenderer.drawStringWithShadow("TASC : " + tasc, k + 160, this.height - 30, 0x00FF00);
+                } else {
+                    fontrenderer.drawStringWithShadow("TASC : off", k + 160, this.height - 30, 0x00FF00);
+                }
 
-				//Limit
+                //Limit
 //				fontrenderer.drawStringWithShadow("Limit: " + atc, k + 160, this.height - 20, 0x00FF00);
-				if (atc == Integer.MAX_VALUE) {
-					fontrenderer.drawStringWithShadow("Limit: ---", k + 160, this.height - 20, 0x00FF00);
-				} else {
-					fontrenderer.drawStringWithShadow("Limit: " + atc, k + 160, this.height - 20, 0x00FF00);
-				}
+                if (atc == Integer.MAX_VALUE) {
+                    fontrenderer.drawStringWithShadow("Limit: ---", k + 160, this.height - 20, 0x00FF00);
+                } else {
+                    fontrenderer.drawStringWithShadow("Limit: " + atc, k + 160, this.height - 20, 0x00FF00);
+                }
 
-				//ATACS
+                //ATACS
 //				fontrenderer.drawStringWithShadow("ATACS: " + atacs, k + 160, this.height - 10, 0x00FF00);
-				if (atacsStatus) {
-					if (atacs == Integer.MAX_VALUE) {
-						fontrenderer.drawStringWithShadow("ATACS: ---", k + 160, this.height - 10, 0x00FF00);
-					} else {
-						fontrenderer.drawStringWithShadow("ATACS: " + atacs, k + 160, this.height - 10, 0x00FF00);
-					}
-				} else {
-					fontrenderer.drawStringWithShadow("ATACS: off", k + 160, this.height - 10, 0x00FF00);
-				}
+                if (atacsStatus) {
+                    if (atacs == Integer.MAX_VALUE) {
+                        fontrenderer.drawStringWithShadow("ATACS: ---", k + 160, this.height - 10, 0x00FF00);
+                    } else {
+                        fontrenderer.drawStringWithShadow("ATACS: " + atacs, k + 160, this.height - 10, 0x00FF00);
+                    }
+                } else {
+                    fontrenderer.drawStringWithShadow("ATACS: off", k + 160, this.height - 10, 0x00FF00);
+                }
 
-			} else {
-				//cab表示なし
+            } else {
+                //cab表示なし
 
-				//ATO
+                //ATO
 //				fontrenderer.drawStringWithShadow("ATO : " + ato, 2, this.height - 90, 16777215);
-				if (atoStatus) {
-					fontrenderer.drawStringWithShadow("ATO : " + ato, 2, this.height - 90, 16777215);
-				} else {
-					fontrenderer.drawStringWithShadow("ATO : off", 2, this.height - 90, 16777215);
-				}
+                if (atoStatus) {
+                    fontrenderer.drawStringWithShadow("ATO : " + ato, 2, this.height - 90, 16777215);
+                } else {
+                    fontrenderer.drawStringWithShadow("ATO : off", 2, this.height - 90, 16777215);
+                }
 
-				//TASC
+                //TASC
 //				fontrenderer.drawStringWithShadow("TASC : " + tasc, 2, this.height - 80, 16777215);
-				if (tascStatus) {
-					fontrenderer.drawStringWithShadow("TASC : " + tasc, 2, this.height - 80, 16777215);
-				} else {
-					fontrenderer.drawStringWithShadow("TASC : off", 2, this.height - 80, 16777215);
-				}
+                if (tascStatus) {
+                    fontrenderer.drawStringWithShadow("TASC : " + tasc, 2, this.height - 80, 16777215);
+                } else {
+                    fontrenderer.drawStringWithShadow("TASC : off", 2, this.height - 80, 16777215);
+                }
 
-				//Limit
+                //Limit
 //				fontrenderer.drawStringWithShadow("Limit : " + atc, 2, this.height - 70, 16777215);
-				if (atc == Integer.MAX_VALUE) {
-					fontrenderer.drawStringWithShadow("Limit : ---", 2, this.height - 70, 16777215);
-				} else {
-					fontrenderer.drawStringWithShadow("Limit : " + atc, 2, this.height - 70, 16777215);
-				}
+                if (atc == Integer.MAX_VALUE) {
+                    fontrenderer.drawStringWithShadow("Limit : ---", 2, this.height - 70, 16777215);
+                } else {
+                    fontrenderer.drawStringWithShadow("Limit : " + atc, 2, this.height - 70, 16777215);
+                }
 
-				//ATACS
+                //ATACS
 //				fontrenderer.drawStringWithShadow("ATACS : " + atacs, 2, this.height - 60, 16777215);
-				if (atacsStatus) {
-					if (atacs == Integer.MAX_VALUE) {
-						fontrenderer.drawStringWithShadow("ATACS : ---", 2, this.height - 60, 16777215);
-					} else {
-						fontrenderer.drawStringWithShadow("ATACS : " + atacs, 2, this.height - 60, 16777215);
-					}
-				} else {
-					fontrenderer.drawStringWithShadow("ATACS : off", 2, this.height - 60, 16777215);
-				}
-			}
-		}
-	}
+                if (atacsStatus) {
+                    if (atacs == Integer.MAX_VALUE) {
+                        fontrenderer.drawStringWithShadow("ATACS : ---", 2, this.height - 60, 16777215);
+                    } else {
+                        fontrenderer.drawStringWithShadow("ATACS : " + atacs, 2, this.height - 60, 16777215);
+                    }
+                } else {
+                    fontrenderer.drawStringWithShadow("ATACS : off", 2, this.height - 60, 16777215);
+                }
+            }
+        }
+    }
 }
