e1b5d4f247cd192990bc5a960188762331be1335
Kaiz_JP <kaiz@kaiz.jp>
2020-04-26 17:31:40 +0900

    1.4.0

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
@@ -22,7 +22,7 @@ public class ATSAssistCore {
 	//変更するとブロック消える
 	public static final String MODID = "ATSAssistMod";
 
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.4.0";
 
 	@Mod.Instance(MODID)
 	public static ATSAssistCore INSTANCE;
