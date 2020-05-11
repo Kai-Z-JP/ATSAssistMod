cda16492017419db23f2f44d66344e14baad3e30
Kaiz_JP <kaiz@kaiz.jp>
2020-05-12 02:58:35 +0900

    保安装置変更対応

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
@@ -11,27 +11,34 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import jp.kaiz.atsassistmod.block.GroundUnit;
+import jp.kaiz.atsassistmod.block.StationAnnounce;
 import jp.kaiz.atsassistmod.event.ATSAssistEventHandler;
 import jp.kaiz.atsassistmod.gui.ATSAssistGUIHandler;
 import jp.kaiz.atsassistmod.item.ItemBlockWithMetadataCustom;
+import jp.kaiz.atsassistmod.item.TrainProtectionSelector;
 import jp.kaiz.atsassistmod.network.*;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 
 @Mod(modid = ATSAssistCore.MODID, version = ATSAssistCore.VERSION, name = ATSAssistCore.MODID)
 public class ATSAssistCore {
 	//変更するとブロック消える
 	public static final String MODID = "ATSAssistMod";
 
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.5beta_v1";
 
 	@Mod.Instance(MODID)
 	public static ATSAssistCore INSTANCE;
 
 	private static int guiId = 0;
 	public static final int guiId_GroundUnit;
+	public static final int guiId_StationAnnounce;
+	public static final int guiId_TrainProtectionSelector;
 
 	public static Block blockGroundUnit;
-//	public static Block stationAnnounceBase;
+	public static Block blockStationAnnounce;
+
+	public static Item itemTrainProtectionSelector;
 
 	@SidedProxy(clientSide = "jp.kaiz.atsassistmod.ClientProxy", serverSide = "jp.kaiz.atsassistmod.CommonProxy")
 	public static CommonProxy proxy;
@@ -41,11 +48,12 @@ public class ATSAssistCore {
 	//preInit init postInitの順
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		NETWORK_WRAPPER.registerMessage(PacketATSAssistKey.class, PacketATSAssistKey.class, 1, Side.SERVER);
+		NETWORK_WRAPPER.registerMessage(PacketSetNotch.class, PacketSetNotch.class, 1, Side.SERVER);
 		NETWORK_WRAPPER.registerMessage(PacketGroundUnitTileInit.class, PacketGroundUnitTileInit.class, 2, Side.SERVER);
 		NETWORK_WRAPPER.registerMessage(PacketGroundUnitTileInitToClient.class, PacketGroundUnitTileInit.class, 3, Side.CLIENT);
 		NETWORK_WRAPPER.registerMessage(PacketGroundUnitTile.class, PacketGroundUnitTile.class, 4, Side.SERVER);
 		NETWORK_WRAPPER.registerMessage(PacketTrainControllerToClient.class, PacketTrainControllerToClient.class, 5, Side.CLIENT);
+		NETWORK_WRAPPER.registerMessage(PacketTrainProtectionSetter.class, PacketTrainProtectionSetter.class, 6, Side.SERVER);
 		NetworkRegistry.INSTANCE.registerGuiHandler(this, new ATSAssistGUIHandler());
 		//proxy噛まさなくてもいいかも
 		proxy.registerTileEntity();
@@ -58,14 +66,22 @@ public class ATSAssistCore {
 	public void preInit(FMLPreInitializationEvent event) {
 		System.out.println("[ATSAssist]Loading...");
 
+		//Block登録
 		blockGroundUnit = new GroundUnit();
 		GameRegistry.registerBlock(blockGroundUnit, ItemBlockWithMetadataCustom.class, "tile" + "." + MODID + ":" + "groundUnit");
-//		stationAnnounceBase = new StationAnnounce();
-//		GameRegistry.registerBlock(stationAnnounceBase, ItemBlockWithMetadataCustom.class, "tile" + "." + MODID + ":" + "stationAnnounceBase");
+		blockStationAnnounce = new StationAnnounce();
+		GameRegistry.registerBlock(blockStationAnnounce, ItemBlockWithMetadataCustom.class, "tile" + "." + MODID + ":" + "stationAnnounceBase");
+
+		//Item登録
+		itemTrainProtectionSelector = new TrainProtectionSelector();
+		GameRegistry.registerItem(itemTrainProtectionSelector, MODID + ":" + "trainProtectionSelector");
+
 		proxy.preInit();
 	}
 
 	static {
 		guiId_GroundUnit = guiId++;
+		guiId_StationAnnounce = guiId++;
+		guiId_TrainProtectionSelector = guiId++;
 	}
 }
