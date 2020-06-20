953af014c4a45d3ce322cf8edeb035d42cc9a7c4
Kaiz_JP <kaiz@kaiz.jp>
2020-06-20 16:52:36 +0900

    ATSAssistSoundManagerが混じっていたのをコメントアウト

--- src/main/java/jp/kaiz/atsassistmod/ClientProxy.java
+++ src/main/java/jp/kaiz/atsassistmod/ClientProxy.java
@@ -2,16 +2,10 @@ package jp.kaiz.atsassistmod;
 
 import cpw.mods.fml.client.FMLClientHandler;
 import jp.kaiz.atsassistmod.event.ATSAssistEventHandlerClient;
-import jp.kaiz.atsassistmod.sound.ATSAssistSoundManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.SoundHandler;
-import net.minecraft.client.audio.SoundRegistry;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-
-import java.lang.reflect.Field;
 
 public class ClientProxy extends CommonProxy {
 
@@ -23,24 +17,24 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void init() {
-        try {
-            ATSAssistSoundManager asm = new ATSAssistSoundManager();
-            SoundHandler soundHandler = Minecraft.getMinecraft().getSoundHandler();
-
-            Field field = soundHandler.getClass().getDeclaredField("field_147697_e");
-            field.setAccessible(true);
-            SoundRegistry sReg = (SoundRegistry) field.get(soundHandler);
-            for (Object key : sReg.getKeys()) {
-                if (key instanceof ResourceLocation) {
-                    ResourceLocation sLoc = (ResourceLocation) key;
-                    asm.addList(sLoc);
-                }
-            }
-            asm.getList().forEach(System.out::println);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-	}
+//        try {
+//            ATSAssistSoundManager asm = new ATSAssistSoundManager();
+//            SoundHandler soundHandler = Minecraft.getMinecraft().getSoundHandler();
+//
+//            Field field = soundHandler.getClass().getDeclaredField("field_147697_e");
+//            field.setAccessible(true);
+//            SoundRegistry sReg = (SoundRegistry) field.get(soundHandler);
+//            for (Object key : sReg.getKeys()) {
+//                if (key instanceof ResourceLocation) {
+//                    ResourceLocation sLoc = (ResourceLocation) key;
+//                    asm.addList(sLoc);
+//                }
+//            }
+//            asm.getList().forEach(System.out::println);
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+    }
 
 	@Override
 	public World getWorld() {
