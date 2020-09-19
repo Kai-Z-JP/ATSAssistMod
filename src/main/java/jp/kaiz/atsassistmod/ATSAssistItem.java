f4be300f37f94b60bd6a3a6ffd36fec8d4642bdc
Kaiz_JP <kaiz@kaiz.jp>
2020-09-19 23:47:46 +0900

    Block DataMap Itemを追加

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistItem.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistItem.java
@@ -1,14 +1,18 @@
 package jp.kaiz.atsassistmod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import jp.kaiz.atsassistmod.item.DataMapEditor;
 import jp.kaiz.atsassistmod.item.TrainProtectionSelector;
 import net.minecraft.item.Item;
 
 public class ATSAssistItem {
 	public static Item itemTrainProtectionSelector;
+	public static Item itemDataMapEditor;
 
 	public void preInit() {
 		ATSAssistItem.itemTrainProtectionSelector = new TrainProtectionSelector();
 		GameRegistry.registerItem(ATSAssistItem.itemTrainProtectionSelector, ATSAssistCore.MODID + ":" + "trainProtectionSelector");
+		ATSAssistItem.itemDataMapEditor = new DataMapEditor();
+		GameRegistry.registerItem(ATSAssistItem.itemDataMapEditor, ATSAssistCore.MODID + ":" + "itemDataMapEditor");
 	}
 }
