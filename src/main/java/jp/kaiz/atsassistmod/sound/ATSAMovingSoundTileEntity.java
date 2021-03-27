9371743b60561bcd2b7d0281b50c1e19459ccb4d
Kaiz_JP <kaiz@kaiz.jp>
2021-03-28 02:01:17 +0900

    ATSAMovingSoundTileEntityの継承元をRTMからMinecraft本体のものに変更

--- src/main/java/jp/kaiz/atsassistmod/sound/ATSAMovingSoundTileEntity.java
+++ src/main/java/jp/kaiz/atsassistmod/sound/ATSAMovingSoundTileEntity.java
@@ -1,24 +1,42 @@
 package jp.kaiz.atsassistmod.sound;
 
-import jp.ngt.rtm.sound.MovingSoundTileEntity;
+import net.minecraft.client.audio.MovingSound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
-public class ATSAMovingSoundTileEntity extends MovingSoundTileEntity {
-    public ATSAMovingSoundTileEntity(TileEntity par1Entity, ResourceLocation par2Sound, boolean par3Repeat) {
-        super(par1Entity, par2Sound, par3Repeat);
+public class ATSAMovingSoundTileEntity extends MovingSound {
+    protected final TileEntity entity;
+
+    public ATSAMovingSoundTileEntity(TileEntity tileEntity, ResourceLocation src, boolean repeat, float volume) {
+        super(src);
+        this.entity = tileEntity;
+        this.setPos(tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord);
+        this.repeat = repeat;
+        this.volume = volume;
     }
 
+    public ATSAMovingSoundTileEntity(TileEntity tileEntity, int[] pos, ResourceLocation src, boolean repeat, float volume) {
+        super(src);
+        this.entity = tileEntity;
+        this.setPos(pos[0], pos[1], pos[2]);
+        this.repeat = repeat;
+        this.volume = volume;
+    }
 
-    public ATSAMovingSoundTileEntity(TileEntity par1Entity, int[] pos, ResourceLocation par2Sound, boolean par3Repeat) {
-        super(par1Entity, par2Sound, par3Repeat);
-        this.xPosF = (float) pos[0] + 0.5F;
-        this.yPosF = (float) pos[1] + 0.5F;
-        this.zPosF = (float) pos[2] + 0.5F;
+    private void setPos(int x, int y, int z) {
+        this.xPosF = (float) x + 0.5F;
+        this.yPosF = (float) y + 0.5F;
+        this.zPosF = (float) z + 0.5F;
     }
 
     @Override
-    public void setVolume(float par1) {
-        this.volume = par1;
+    public void update() {
+        if (this.entity.isInvalid()) {
+            this.donePlaying = true;
+        }
+    }
+
+    public void stop() {
+        this.donePlaying = true;
     }
 }
