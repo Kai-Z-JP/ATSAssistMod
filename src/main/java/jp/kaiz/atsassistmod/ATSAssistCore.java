3bcebf926911f37c2d06bab777988cda542e995c
Kaiz <kaiz@kaiz.jp>
2021-09-04 23:03:25 +0900

    Update version to 1.5.0

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
@@ -15,7 +15,7 @@ public class ATSAssistCore {
     //変更するとブロック消える
     public static final String MODID = "ATSAssistMod";
 
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
 
     @Mod.Instance(MODID)
     public static ATSAssistCore INSTANCE;
