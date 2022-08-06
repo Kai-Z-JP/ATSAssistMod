1507bb800123a807150d877bac80d4d8a8291141
Kaiz <kaiz@kaiz.jp>
2022-08-07 00:28:18 +0900

    Bump up version to 1.7.2

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
@@ -15,7 +15,7 @@ public class ATSAssistCore {
     //変更するとブロック消える
     public static final String MODID = "ATSAssistMod";
 
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.2";
 
     @Mod.Instance(MODID)
     public static ATSAssistCore INSTANCE;
