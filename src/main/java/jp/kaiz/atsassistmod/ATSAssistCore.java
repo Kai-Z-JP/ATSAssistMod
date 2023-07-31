c67a44fff2df1c51f4cb65c19931bfee36ca718d
Kaiz <kaiz@kaiz.jp>
2023-07-31 11:49:53 +0900

    chore(release): publish v1.7.7

--- src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
+++ src/main/java/jp/kaiz/atsassistmod/ATSAssistCore.java
@@ -14,7 +14,7 @@ public class ATSAssistCore {
     //変更するとブロック消える
     public static final String MODID = "atsassistmod";
 
-    public static final String VERSION = "1.7.6";
+    public static final String VERSION = "1.7.7";
 
     @Mod.Instance(MODID)
     public static ATSAssistCore INSTANCE;
